9009379d2072246751db122fc2661fa70773ff72
praju-jagtap <prajaktajagtap2412@gmail.com>
2022-09-23 20:14:56 +0530

    [Prajakta] Add: Display Welcome Message

---
+++ ValidationEmail/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace ValidationEmail
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome To Sample Email Validation Problem statement");
+        }
+    }
+}
